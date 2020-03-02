74e646248efe08f32b1b9e410690ac8ade0ed430
ozmen <ozmen@sabanciuniv.edu>
2020-03-02 18:08:23 +0300

    initial commit

---
+++ tutorial4/src/tutorial4/tutorial4.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package tutorial4;
+
+/**
+ * @author poyraz
+ *
+ */
+public class tutorial4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
