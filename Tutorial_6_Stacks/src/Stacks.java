bd1aca6b54c23bfde1c07c6333a66531bd40e627
ozmen <ozmen@sabanciuniv.edu>
2020-03-02 23:57:08 +0300

    stacks

---
+++ Tutorial_6_Stacks/src/Stacks.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+
+public class Stacks {
+
+	public static void main(String[] args) {
+		LinkedList<String> names = new LinkedList<String>();
+		// instead of adding we will use push for stacks names.add("Caleb");
+		names.push("Caleb");
+		names.push("Sue");
+		names.push("Sally");
+		
+		// same idea we will use pop, System.out.println(names.remove());
+		System.out.println(names.pop());
+		System.out.println(names.remove());
+		System.out.println(names.removeFirst()); // pop and RemoveFirst are equilavent
+		// with remove we can use index, by default it removes last, like remove(4)
+		// when it is executed Result is Sally-Sue-Caleb
+	}
+
+}
