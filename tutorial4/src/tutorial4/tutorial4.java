ef978500905d68ec5d01d1b40bbd8be037c686d3
ozmen <ozmen@sabanciuniv.edu>
2020-03-02 23:44:41 +0300

    about collection interfaces

--- tutorial4/src/tutorial4/tutorial4.java
+++ tutorial4/src/tutorial4/tutorial4.java
@@ -5,6 +5,8 @@ package tutorial4;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 /**
@@ -13,18 +15,20 @@ import java.util.Scanner;
  */
 public class tutorial4 {
 
-	/**
-	 * @param args
-	 * @throws FileNotFoundException 
-	 */
+	Collection<String> stuff;
+	
 	public static void main(String[] args) throws FileNotFoundException {
 		
 		Scanner in = new Scanner(new File("students.txt"));
 		List<String> students = new ArrayList<String>();
+		
+		// you can use collections <String> students = new ArrayList<String>();
 		// left side we use list, on the right we used implementation of list
 		// arraylist is collection of list
 		// left side is collection interface
 		// check collection on dosc.oracle-java collection 
+		
+		
 		
 		while(in.hasNextLine()) {
 			students.add(in.nextLine());
@@ -35,5 +39,10 @@ public class tutorial4 {
 		for (int i = 0; i<students.size(); i++) {
 			System.out.println(students.get(i));
 		}
+		tutorial4 p = new tutorial4();
+		p.stuff = new LinkedList<String>();
+		
+		// we did not get any errors because stuff is implemented as a collection 
+		// so we can return any type of the collection
 	}
 }
